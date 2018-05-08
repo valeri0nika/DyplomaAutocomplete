28ee382ba38d4d0990e5bb9e8428b25f732242f3
Valery <valeri0nika.viva@gmail.com>
2018-05-08 21:05:03 +0300

    Inital project

---
+++ exportcode.js
@@ -0,0 +1,167 @@
+'use strict()'; //Global libs
+//debug
+var util = require('util');
+
+//var memProfile = require('memoizee/profile'); // debug
+var Promise = require('bluebird');
+var memoize = require('memoizee');
+var _ = require('lodash');
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var logger = require('tracer').dailyfile({
+  root: '.',
+});
+
+//My libs
+var codeRepo = require('./services/code_repo');
+
+var restify = require('restify');
+
+//Server config
+var server = restify.createServer({
+  name: 'abbottpm-code',
+  version: '1.0.0',
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.gzipResponse());
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS({
+  origins: ['http://abbottpm.com'],
+}));
+
+var allMem = memoize(codeRepo.all);
+var autoNameMem = memoize(codeRepo.autoName);
+var autoCodeMem = memoize(codeRepo.autoCode);
+var moreInMem = memoize(codeRepo.moreIn);
+
+server.get('/rest/all', function(req, res, next) {
+  var year = 2012;
+  if (req.query.year) {
+    year = req.query.year;
+  }
+
+  allMem('v-1', year)
+    .then(function(result) {
+      //return res.send(result);
+      var arr = result.map(function(node) {
+        return allMem(node._key, year); //.then(function(r){ return r});
+      });
+
+      Promise.settle(arr).then(function(ress) {
+        var all = result.map(function(node, index) {
+          var nwc = node;
+          nwc['children'] = ress[index].value();
+          return nwc;
+        });
+
+        res.send(all);
+      });
+    }).catch(function(error) {
+      logger.error(error);
+      return res.send(error);
+    });
+
+});
+
+server.get('/rest/auto/:name', function(req, res, next) {
+  var name = req.params.name;
+  if (!name) {
+    next();
+  }
+
+  autoNameMem(name).then(function(results) {
+      return res.send(results);
+    }).catch(function(err) {
+      console.log(err);
+      return res.send([]);
+    });
+});
+
+server.get('/rest/more', function(req, res, next) {
+  var id = req.query.id;
+  if (!id) {
+    next();
+  }
+
+  moreInMem(id).then(function(result) {
+    _.flattenDeep(result).map(function(v) {
+      v.children.sort(compare);
+    });
+
+    return res.send(_.flattenDeep(result));
+
+  }).catch(function(err) {
+    logger.error(err);
+    //return res.send([]);
+  });
+
+});
+
+server.get('/rest/find', function(req, res, next) {
+  var name = req.query.name;
+  var code = req.query.code;
+
+
+
+  logger.info(name);
+  logger.info(code);
+  if (name) { //do code
+    codeRepo.findName(name).then(function(result) {
+      //console.log(result);
+      //var arr = result.map(function(v) {
+      var core = _.chain(result[0].vertices).tail().value();
+      var first = _.first(core);
+      //console.log(core);
+      first['children'] = [];
+      first['children'] = core[1];
+      //});
+      var ress = [first];
+      return res.send(ress);
+    }).catch(function(err) {
+      logger.error(err);
+      return res.send([]);
+    });
+  }
+
+  if (code) { //do code
+    codeRepo.findCode(code).then(function(result) {
+      //console.log(result[0]);
+      var core = _.chain(result[0].vertices).tail().value();
+      var first = _.first(core);
+      first['children'] = [];
+      first['children'].push(core[1]);
+
+      //return first;
+      //});
+
+      //console.log(arr);
+      var ress = [first];
+      return res.send(ress);
+    }).catch(function(err) {
+      logger.error(err);
+    });
+  }
+
+  next();
+
+});
+
+/*Utilit*/
+function compare(a, b) {
+  if (a.code < b.code)
+    return -1;
+  if (a.code > b.code)
+    return 1;
+  return 0;
+}
+
+server.listen(5005, '0.0.0.0', function() {
+
+  var host = server.address().address;
+  var port = server.address().port;
+  allMem.clear();
+  moreInMem.clear();
+  console.log('ExportCode app listening at http://%s:%s', host, port);
+
+});
---
+++ services/code_repo.js
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var db = require('arangojs')({
+  arangoVersion: 20800
+
+});
+var Promise = require('bluebird');
+
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
+
+
+module.exports.all = function(node, year) {
+  var query = 'for e IN GRAPH_NEIGHBORS (\'exports\', ' +
+    '{_key:@node}, ' +
+    '{maxDepth:1, includeData:true, direction:\'inbound\', neighborExamples:{year:2012}})  ' +
+    'SORT e.code return e';
+  return getData(query, node, year);
+};
+
+module.exports.moreIn = function(node) {
+  var query =
+    'for e IN GRAPH_TRAVERSAL_TREE(\'exports\', {_key:@node}, ' +
+    ' \'inbound\' ,  \'children\',{uniqueness:"global"}) SORT e.code return e';
+  return getData(query, node, '');
+};
+
+module.exports.autoName = function(name) {
+  name = 'prefix:' + name;
+
+//////////////////////
+  var query = 'for e IN  FULLTEXT(export_nodes,\'name\', \'' + name + '\') \
+                    FILTER e.code != ""\
+                    LIMIT 10 return {"term": CONCAT(e.name,\'(\', e.code, \')\' ), "code":e.code, "id":e.id}';
+  return getData(query, name, '');
+};
+
+module.exports.autoCode = function(code) {
+  code = 'prefix:' + code;
+
+  var query =
+    'for e IN FULLTEXT(export_nodes,\'code\', \'' + code + '\')  limit 10 return  {"term": CONCAT(e.code,\'(\', e.name, \')\' ), "code":e.code, "id":e.id}';
+  return getData(query, code, '');
+};
+
+module.exports.findName = function(name) {
+  name = 'prefix:' + name;
+
+  var query =
+    'for e IN FULLTEXT(export_nodes,\'name\',@node)  for n in GRAPH_SHORTEST_PATH(\'exports\', {_key:\'v-1\'}, e, {includeData: true}) return n';
+  return getData(query, name, '');
+};
+
+module.exports.findCode = function(code) {
+  code = 'prefix:' + code;
+
+  var query =
+    'for e IN FULLTEXT(export_nodes,\'code\',@node)  for n in GRAPH_SHORTEST_PATH(\'exports\', {_key:\'v-1\'}, e, {includeData: true}) return n';
+  return getData(query, code, '');
+};
+
+//Query noSQL
+function getData(query, param, year) {
+  return db.query(query).then(function(cursor) {
+    return cursor.all()
+  });
+}
